dcaf2e6b87ed69313e790ec3050eca50fb19a714
Davidsb04 <caisilvasb756@hotmail.com>
2024-04-04 19:55:01 -0300

    Inicialização do Projeto

---
+++ Cerimonia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_TrabalhoPratico
+{
+    internal class Cerimonia
+    {
+        private DateTime Data {  get; set; }
+        private int NumConvidados { get; set; }
+        private Espaco espaco {  get; set; }
+
+        public Cerimonia(DateTime data, int NumConvidados, Espaco espaco)
+        {
+
+        }
+    }
+}
